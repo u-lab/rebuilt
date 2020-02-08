f9802713eff0537df334ab15a32a10dd587a4dfd
H37kouya <h37kouya@gmail.com>
2020-02-08 10:55:12 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const { join } = require('path')
+const { copySync, removeSync } = require('fs-extra')
+
+module.exports = {
+  srcDir: __dirname,
+
+  env: {
+    apiUrl: process.env.API_URL,
+    appName: process.env.APP_NAME,
+    appLocale: process.env.APP_LOCALE || 'en',
+    githubAuth: !!process.env.GITHUB_CLIENT_ID
+  },
+
+  head: {
+    title: process.env.APP_NAME,
+    titleTemplate: '%s - ' + process.env.APP_NAME,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  loading: { color: '#007bff' },
+
+  router: {
+    middleware: ['locale', 'check-auth']
+  },
+
+  css: [
+    { src: '~assets/sass/app.scss', lang: 'scss' }
+  ],
+
+  plugins: [
+    '~components/global',
+    '~plugins/i18n',
+    '~plugins/vform',
+    '~plugins/axios',
+    '~plugins/fontawesome',
+    { src: '~plugins/bootstrap', mode: 'client' }
+  ],
+
+  modules: [
+    '@nuxtjs/router'
+  ],
+
+  build: {
+    extractCSS: true
+  },
+
+  hooks: {
+    generate: {
+      done (generator) {
+        // Copy dist files to public/_nuxt
+        if (generator.nuxt.options.dev === false && generator.nuxt.options.mode === 'spa') {
+          const publicDir = join(generator.nuxt.options.rootDir, 'public', '_nuxt')
+          removeSync(publicDir)
+          copySync(join(generator.nuxt.options.generate.dir, '_nuxt'), publicDir)
+          copySync(join(generator.nuxt.options.generate.dir, '200.html'), join(publicDir, 'index.html'))
+          removeSync(generator.nuxt.options.generate.dir)
+        }
+      }
+    }
+  }
+}
---
+++ router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { scrollBehavior } from '~/utils'
+
+Vue.use(Router)
+
+const page = path => () => import(`~/pages/${path}`).then(m => m.default || m)
+
+const routes = [
+  { path: '/', name: 'welcome', component: page('welcome.vue') },
+
+  { path: '/login', name: 'login', component: page('auth/login.vue') },
+  { path: '/register', name: 'register', component: page('auth/register.vue') },
+  { path: '/password/reset', name: 'password.request', component: page('auth/password/email.vue') },
+  { path: '/password/reset/:token', name: 'password.reset', component: page('auth/password/reset.vue') },
+  { path: '/email/verify/:id', name: 'verification.verify', component: page('auth/verification/verify.vue') },
+  { path: '/email/resend', name: 'verification.resend', component: page('auth/verification/resend.vue') },
+
+  { path: '/home', name: 'home', component: page('home.vue') },
+  { path: '/settings',
+    component: page('settings/index.vue'),
+    children: [
+      { path: '', redirect: { name: 'settings.profile' } },
+      { path: 'profile', name: 'settings.profile', component: page('settings/profile.vue') },
+      { path: 'password', name: 'settings.password', component: page('settings/password.vue') }
+    ] }
+]
+
+export function createRouter () {
+  return new Router({
+    routes,
+    scrollBehavior,
+    mode: 'history'
+  })
+}
