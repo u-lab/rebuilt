e572e3bfd336859be1ae6edd3b82af473445d8dd
H37kouya <h37kouya@gmail.com>
2020-02-10 22:27:26 +0900

    [add]: apiを叩いた

--- pages/pages/_user.vue
+++ pages/pages/_user.vue
@@ -5,7 +5,39 @@
 </template>
 
 <script>
+import axios from 'axios'
+import Form from 'vform'
+
 export default {
 
+  data () {
+    return {
+      form: new Form({
+        data: ''
+      })
+    }
+  },
+
+  async asyncData ({ params, error }) {
+    try {
+      const { data } = await axios.get(`pages/${params.user}`)
+      return { success: true, data }
+    } catch (e) {
+      // return error({
+      //   statusCode: e.response.data.status,
+      //   message: e.response.data.message
+      // })
+      return { success: false, statusCode: e.response.data.status }
+    }
+  },
+
+  created () {
+      // Fill the form with data.
+      this.form.keys().forEach((key) => {
+        if (this.data[key] !== null) {
+          this.form[key] = this.data[key]
+        }
+      })
+  }
 }
 </script>
