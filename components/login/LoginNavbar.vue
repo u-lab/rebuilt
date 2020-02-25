ff25fdd20b1e5744c601ac1089364391e87582d6
H37kouya <h37kouya@gmail.com>
2020-02-25 10:36:44 +0900

    ログインページの作成

---
+++ components/login/LoginNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="transparent" flat>
+    <v-spacer />
+    <locale-dropdown />
+  </v-app-bar>
+</template>
+
+<script>
+import LocaleDropdown from '~/components/LocaleDropdown'
+
+export default {
+  components: {
+    LocaleDropdown
+  }
+}
+</script>
---
+++ layouts/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <login-navbar />
+
+    <!-- Sizes your content based upon application components -->
+    <v-content class="login-container d-flex align-center">
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import LoginNavbar from '~/components/login/LoginNavbar'
+
+export default {
+  components: {
+    LoginNavbar
+  }
+}
+</script>
