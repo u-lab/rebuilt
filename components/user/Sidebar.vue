03755aa0a0774653bfe8b52364d561a805deb1bc
H37kouya <h37kouya@gmail.com>
2020-02-25 18:44:24 +0900

    サイドバーの項目の修正

--- components/user/Sidebar.vue
+++ components/user/Sidebar.vue
@@ -12,10 +12,16 @@
         :key="item.name"
         nuxt
         link
-        :to="item.link"
+        :to="generateLink(item)"
       >
         <v-list-item-content>
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+
+          <v-list-item v-for="subtitle in item.subtitles" :key="subtitle.title" link nuxt :to="generateLink(subtitle)">
+            <v-list-item-title>
+              {{ subtitle.title }}
+            </v-list-item-title>
+          </v-list-item>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -33,19 +39,48 @@ export default {
   computed: {
     getLinkList: () => {
       return [
-        { name: 'Top Page', link: '/' },
-        { name: '個人ページ', link: '/pages/admin' },
-        { name: '作品一覧', link: '/pages/admin/storages' },
-        { name: '作品ページ', link: '/pages/admin/storages/1581315433ra05d0' },
-        { name: 'プロフィール一覧', link: '/profiles' },
-        { name: '作品一覧', link: '/storages' },
-        { name: '自分のポートフォリオ', link: '/users/page' },
-        { name: '自分のポートフォリオ(編集)', link: '/users/page/edit' },
-        { name: '自分の作品一覧', link: '/users/storages' },
-        { name: '新規作品追加', link: '/users/storages/create' },
-        { name: '自分の作品を見る', link: '/users/storages/1581315433ra05d0' },
-        { name: '自分の作品(編集)', link: '/users/storages/1581315433ra05d0/edit' }
-      ]
+          { title: 'ダッシュボード', name: 'users.dashboard' },
+          {
+            title: '作品を記録する',
+            subtitles: [
+              { title: '作品の投稿', name: 'users.storages.create' },
+              { title: '作品一覧', name: 'users.storages.index' },
+              { title: '作品(デバッグ用)', link: '/users/storages/1581315433ra05d0' },
+              { title: '作品編集(デバッグ用)', link: '/users/storages/1581315433ra05d0/edit' }
+            ]
+          },
+          {
+            title: '作品を眺める',
+            subtitles: [
+              { title: 'みんなのプロフィールを見る', name: 'users.profiles' },
+              { title: 'みんなの作品を見る', name: 'users.storages' }
+            ]
+          },
+          {
+            title: 'アカウント設定',
+            subtitles: [
+              { title: 'ポートフォリオを見る', name: 'users.page.index' },
+              { title: 'ポートフォリオを編集', name: 'users.page.edit' },
+              { title: 'アカウント情報を見る', name: 'users.profile.index' },
+              { title: 'アカウント情報を編集', name: 'users.profile.edit' }
+            ]
+          }
+        ]
+    }
+
+  },
+
+  methods: {
+    generateLink (item) {
+      if (item.hasOwnProperty('name')) {
+        return { name: item.name }
+      }
+
+      if (item.hasOwnProperty('link')) {
+        return item.link
+      }
+
+      return null
     }
   }
 }
