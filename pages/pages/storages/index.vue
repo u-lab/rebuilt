d5aab30b4d78d3d4b384870420e2d54e4a32e3f1
toson <theawerqwer@gmail.com>
2020-02-26 16:57:02 +0900

    index.vue asyncdata作成

--- pages/pages/storages/index.vue
+++ pages/pages/storages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <!-- TODO 作品一覧を作る -->
+    <h1>こんにちは</h1>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get("pages/admin/storages");
+
+    return { success: true, data: data.data, user: data.user };
+  }
+};
 </script>
