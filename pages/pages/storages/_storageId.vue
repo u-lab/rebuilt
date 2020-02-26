5a1a188db4611091b84dcb476c30df39ce4183b3
toson <theawerqwer@gmail.com>
2020-02-26 12:25:36 +0900

    作品ページ外枠できた

--- pages/pages/storages/_storageId.vue
+++ pages/pages/storages/_storageId.vue
@@ -2,30 +2,38 @@
   <div>
     <!-- TODO 作品紹介ページ(1作品を紹介) -->
     <v-app>
-      <v-card height="500px" color="#E3F2FD" class="stl_viewer">
-        <v-container fill-height>
-          <v-layout>
-            <v-flex align-center>
-              <v-img height="400px" contain :src="work2Img"></v-img>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
+      <v-container class="stl_viewer_parent">
+        <v-card height="500px" color="#E3F2FD" class="stl_viewer">
+          <v-container fill-height>
+            <v-layout>
+              <v-flex class="align-center py-6">
+                <v-img height="100%" contain :src="work2Img" />
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-container>
       <v-container class="example">
         <v-card color="#FFF8E1">
-          <v-row class="user">
-            <v-col>
-              <v-avatar>
-                <img
-                  src="https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61.jpg?s=200&d=mm"
-                />
-              </v-avatar>
-            </v-col>
-            <v-col>
-              <h2>{{user.name}}</h2>
-            </v-col>
-          </v-row>
-          <v-container class="contents">
+          <v-container class="mainhead px-10 pb-0">
+            <v-row class="worktitle">
+              <v-card-title>TrialHome</v-card-title>
+            </v-row>
+            <v-row class="user">
+              <v-col cols="1">
+                <v-avatar>
+                  <img
+                    src="https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61.jpg?s=200&d=mm"
+                  />
+                </v-avatar>
+              </v-col>
+              <v-col cols="11">
+                <h2>{{user.name}}</h2>
+                <p>{{subusername}}</p>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-container class="contents px-10">
             <v-row>
               <v-col cols="6">
                 <v-container class="left_contents">
@@ -33,32 +41,57 @@
                     <v-img height="400px" contain :src="work1Img"></v-img>
                   </v-row>
                   <v-row class="spePerctives">
-                    <v-container>
-                      <v-layout>
-                        <v-flex class="flex-row">
-                          <v-img width="100px" :src="work2Img"></v-img>
-
-                          <v-img width="100px" :src="work2Img"></v-img>
-
-                          <v-img width="100px" :src="work2Img"></v-img>
-
-                          <v-img width="100px" :src="work2Img"></v-img>
-
-                          <v-img width="100px" :src="work2Img"></v-img>
-                        </v-flex>
-                      </v-layout>
-                    </v-container>
+                    <v-col cols="3">
+                      <v-img width="100px" contain :src="work2Img"></v-img>
+                    </v-col>
+                    <v-col cols="3">
+                      <v-img width="100px" contain :src="work2Img"></v-img>
+                    </v-col>
+                    <v-col cols="3">
+                      <v-img width="100px" contain :src="work2Img"></v-img>
+                    </v-col>
+                    <v-col cols="3">
+                      <v-img width="100px" contain :src="work2Img"></v-img>
+                    </v-col>
                   </v-row>
                 </v-container>
               </v-col>
-              <!--<v-container class="right_contents"-->
-              <v-col cols="6" class="comments">
+              <v-col cols="6" class="comments pt-10">
                 <v-card>
                   <v-card-title>comments</v-card-title>
+                  <v-card-text>最近自然の中の曲線や形に興味がある。特にネジバナやサボテンに見られる”螺旋”やバッタの脚を始めとした昆虫の足の曲線が凄く不思議に心が惹かれる。そこで、そういった自然的な形や線をロボットに入れ込むことで、naturalなロボットを作ることができるのかについて考えた。</v-card-text>
                 </v-card>
               </v-col>
-              <!--/v-container-->
             </v-row>
+          </v-container>
+          <v-container class="other-works">
+            <v-row class="other-work-title px-5">
+              <v-card-title>other works</v-card-title>
+            </v-row>
+            <v-container class="pa-0">
+              <v-row>
+                <v-col cols="3">
+                  <v-container class="fill-height">
+                    <v-img width="200px" contain :src="work2Img"></v-img>
+                  </v-container>
+                </v-col>
+                <v-col cols="3">
+                  <v-container class="fill-height">
+                    <v-img width="200px" contain :src="work2Img"></v-img>
+                  </v-container>
+                </v-col>
+                <v-col cols="3">
+                  <v-container class="fill-height">
+                    <v-img width="200px" contain :src="work2Img"></v-img>
+                  </v-container>
+                </v-col>
+                <v-col cols="3">
+                  <v-container class="fill-height">
+                    <v-img width="200px" contain :src="work2Img"></v-img>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
           </v-container>
         </v-card>
       </v-container>
@@ -83,7 +116,8 @@ export default {
   data() {
     return {
       work1Img: work1Img,
-      work2Img: work2Img
+      work2Img: work2Img,
+      subusername: "admin"
     };
   },
   computed: {
