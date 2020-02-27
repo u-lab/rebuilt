6742142d629077ebb10e306c797d2f07b88ab6ee
toson <theawerqwer@gmail.com>
2020-02-27 16:17:29 +0900

    strageld.vue autofix

--- pages/pages/storages/_storageId.vue
+++ pages/pages/storages/_storageId.vue
@@ -4,7 +4,7 @@
     <v-container class="stl-viewer-parent">
       <v-card height="500px" color="#E3F2FD" class="stl-viewer">
         <v-container style="height: 100%" class="text-center pa-10">
-          <v-img height="100%" contain :src="work2Img"></v-img>
+          <v-img :src="work2Img" height="100%" contain></v-img>
         </v-container>
       </v-card>
     </v-container>
@@ -12,7 +12,7 @@
       <v-card color="#FFF8E1">
         <v-container class="mainhead px-10 pb-0">
           <v-row class="worktitle">
-            <v-card-title>{{worktitle}}</v-card-title>
+            <v-card-title>{{ worktitle }}</v-card-title>
           </v-row>
           <v-row class="user">
             <v-col cols="1">
@@ -23,8 +23,8 @@
               </v-avatar>
             </v-col>
             <v-col cols="11">
-              <h2>{{user.name}}</h2>
-              <p>{{subusername}}</p>
+              <h2>{{ user.name }}</h2>
+              <p>{{ subusername }}</p>
             </v-col>
           </v-row>
         </v-container>
@@ -33,20 +33,20 @@
             <v-col cols="6">
               <v-container class="left-contents">
                 <v-row>
-                  <v-img height="400px" contain :src="work1Img"></v-img>
+                  <v-img :src="work1Img" height="400px" contain></v-img>
                 </v-row>
                 <v-row class="perspectives">
                   <v-col cols="3">
-                    <v-img width="100px" contain :src="work2Img"></v-img>
+                    <v-img :src="work2Img" width="100px" contain></v-img>
                   </v-col>
                   <v-col cols="3">
-                    <v-img width="100px" contain :src="work2Img"></v-img>
+                    <v-img :src="work2Img" width="100px" contain></v-img>
                   </v-col>
                   <v-col cols="3">
-                    <v-img width="100px" contain :src="work2Img"></v-img>
+                    <v-img :src="work2Img" width="100px" contain></v-img>
                   </v-col>
                   <v-col cols="3">
-                    <v-img width="100px" contain :src="work2Img"></v-img>
+                    <v-img :src="work2Img" width="100px" contain></v-img>
                   </v-col>
                 </v-row>
               </v-container>
@@ -54,7 +54,7 @@
             <v-col cols="6" class="comments pt-10">
               <v-card>
                 <v-card-title>Comments</v-card-title>
-                <v-card-text>{{comments}}</v-card-text>
+                <v-card-text>{{ comments }}</v-card-text>
               </v-card>
             </v-col>
           </v-row>
@@ -67,22 +67,22 @@
             <v-row>
               <v-col cols="3">
                 <v-container class="fill-height">
-                  <v-img width="200px" contain :src="work2Img"></v-img>
+                  <v-img :src="work2Img" width="200px" contain></v-img>
                 </v-container>
               </v-col>
               <v-col cols="3">
                 <v-container class="fill-height">
-                  <v-img width="200px" contain :src="work2Img"></v-img>
+                  <v-img :src="work2Img" width="200px" contain></v-img>
                 </v-container>
               </v-col>
               <v-col cols="3">
                 <v-container class="fill-height">
-                  <v-img width="200px" contain :src="work2Img"></v-img>
+                  <v-img :src="work2Img" width="200px" contain></v-img>
                 </v-container>
               </v-col>
               <v-col cols="3">
                 <v-container class="fill-height">
-                  <v-img width="200px" contain :src="work2Img"></v-img>
+                  <v-img :src="work2Img" width="200px" contain></v-img>
                 </v-container>
               </v-col>
             </v-row>
@@ -94,26 +94,25 @@
 </template>
 
 <script>
-import axios from "axios";
-import work1Img from "~/assets/img/work1.jpg";
-import work2Img from "~/assets/img/work2.jpg";
+import axios from 'axios'
+import work1Img from '~/assets/img/work1.jpg'
+import work2Img from '~/assets/img/work2.jpg'
 
 export default {
-  async asyncData({ params }) {
-    const { data } = await axios.get("pages/admin/storages/1581315433ra05d0");
-
-    return { success: true, data: data.data, user: data.user };
-  },
-
   data() {
     return {
-      work1Img: work1Img,
-      work2Img: work2Img,
-      subusername: "admin",
-      worktitle: "TraialHome",
+      work1Img,
+      work2Img,
+      subusername: 'admin',
+      worktitle: 'TraialHome',
       comments:
-        "最近自然の中の曲線や形に興味がある。特にネジバナやサボテンに見られる”螺旋”やバッタの脚を始めとした昆虫の足の曲線が凄く不思議に心が惹かれる。そこで、そういった自然的な形や線をロボットに入れ込むことで、naturalなロボットを作ることができるのかについて考えた。"
-    };
+        '最近自然の中の曲線や形に興味がある。特にネジバナやサボテンに見られる”螺旋”やバッタの脚を始めとした昆虫の足の曲線が凄く不思議に心が惹かれる。そこで、そういった自然的な形や線をロボットに入れ込むことで、naturalなロボットを作ることができるのかについて考えた。'
+    }
+  },
+  async asyncData({ params }) {
+    const { data } = await axios.get('pages/admin/storages/1581315433ra05d0')
+
+    return { success: true, data: data.data, user: data.user }
   }
-};
+}
 </script>
