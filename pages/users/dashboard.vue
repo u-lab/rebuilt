a3ec16794d852f24be684a2cf380f55203903808
H37kouya <h37kouya@gmail.com>
2020-03-04 15:25:33 +0900

    dashboardのデザインを一部作った

--- pages/users/dashboard.vue
+++ pages/users/dashboard.vue
@@ -2,10 +2,71 @@
   <div>
     <!-- TODO ユーザーダッシュボード -->
     <user-title :title="$t('dashboard')" />
+
+    <v-card>
+      <v-card-title>Archi Walkへようこそ</v-card-title>
+    </v-card>
+
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>作品を記録する</v-card-title>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>作品を眺める</v-card-title>
+
+          <v-list dense>
+            <v-list-item-group>
+              <v-list-item
+                v-for="item in showItems(showItemNum)"
+                :key="item.storage_id"
+                :to="{
+                  name: 'users.storages.show',
+                  params: { storageId: item.storage_id }
+                }"
+                link
+                nuxt
+              >
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  v-if="
+                    item.eyecatch_image &&
+                      item.eyecatch_image.hasOwnProperty('url_160')
+                  "
+                >
+                  <v-img
+                    v-if="item.eyecatch_image.url_160"
+                    :src="item.eyecatch_image.url_160"
+                  ></v-img>
+                  <v-img v-else :src="item.eyecatch_image.url"></v-img>
+                </v-list-item-avatar>
+              </v-list-item>
+            </v-list-item-group>
+
+            <template v-if="showItemNum !== maxShowItmeNum">
+              <v-divider></v-divider>
+
+              <v-list-item @click="showItemNum = maxShowItmeNum" link>
+                <v-list-item-content>
+                  <v-list-item-title>もっと見る</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import UserTitle from '~/components/user/UserTitle'
 
 export default {
@@ -15,6 +76,40 @@ export default {
 
   components: {
     UserTitle
+  },
+
+  data() {
+    return {
+      showItemNum: 5
+    }
+  },
+
+  computed: {
+    maxShowItmeNum() {
+      const len = this.data.data.length
+
+      return len < 10 ? len : 10
+    },
+
+    showItems() {
+      return function(num) {
+        return this.data.data.slice(0, num)
+      }
+    }
+  },
+
+  async asyncData() {
+    const { data } = await axios.get('users/storage', {
+      params: {
+        per_page: 10
+      }
+    })
+
+    return { success: true, data }
+  },
+
+  created() {
+    this.showItemNum = this.maxShowItmeNum < 5 ? this.maxShowItmeNum : 5
   }
 }
 </script>
