09f8c4346fe9572f39ca52e5a01e08c56717b939
shuwa0216 <shuwa0216@yahoo.co.jp>
2020-03-13 14:54:55 +0900

    Setup for AWS CodePipeline

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo chown ec2-user:ec2-user /home/ec2-user/rebuilt
+cp -r /home/ec2-user/rebuilt/components/ /home/ec2-user/
+cd /home/ec2-user/rebuilt
+result=`ps aux | grep node | head -n 1`
+list=(${result// / })
+num=${list[1]}
+echo `kill -9 $num`
+echo `npm install`
+echo `npm run dev` &
