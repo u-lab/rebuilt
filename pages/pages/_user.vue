d393944046ede7140fe705092a9fd3bb5bc05a65
H37kouya <h37kouya@gmail.com>
2020-03-17 20:10:44 +0900

    タブのデザインの追加

--- pages/pages/_user.vue
+++ pages/pages/_user.vue
@@ -1,29 +1,84 @@
 <template>
   <div>
+    <!-- kanaをjobにしてる。デザインを見て直すか決める -->
     <user-header
       :bgSrc="BgExampleImg"
-      :iconSrc="myIcon"
-      :name="data.name"
-      kana="FURIGANA"
+      :iconSrc="getIconUrl"
+      :name="data.user_profile.nick_name"
+      :kana="data.user_profile.job_name"
     />
+
+    <v-container>
+      <v-card>
+        <v-tabs v-model="tab" dark grow slider-color="white">
+          <v-tab>
+            - work -
+          </v-tab>
+          <v-tab>
+            - profile -
+          </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <v-card flat>
+              <tab-box title="Storage" content="作品を入れる" />
+            </v-card>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-card flat>
+              <tab-box title="History" content="あああ" />
+
+              <tab-box title="Award" content="いいい" />
+
+              <tab-box
+                :content="data.user_profile.description"
+                :title="$t('description')"
+              />
+
+              <tab-box
+                :content="data.user_profile.hobby"
+                :title="$t('hobby')"
+              />
+
+              <tab-box
+                :content="data.user_profile.web_address"
+                title="My Site"
+              />
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import UserHeader from '@/components/pages/UserHeader'
+import TabBox from '@/components/pages/TabBox'
 import BgExampleImg from '@/assets/img/page_background_example.png'
 import myIcon from '~/assets/img/usericon-ex.jpg'
 
 export default {
   components: {
-    UserHeader
+    UserHeader,
+    TabBox
   },
 
   data() {
     return {
+      tab: null,
       BgExampleImg,
       myIcon
+    }
+  },
+
+  computed: {
+    getIconUrl() {
+      const image = this.data.user_profile.icon_image
+      return image.url_160 || image.url_320 || image.url
     }
   },
 
