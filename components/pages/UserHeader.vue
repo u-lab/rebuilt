df46ed374a1c782feba05e8751b26ffdc4054394
H37kouya <h37kouya@gmail.com>
2020-03-17 18:30:43 +0900

    pages/:userのheaderぽいところを作成

---
+++ components/pages/UserHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="pages-user-header">
+    <v-img :src="bgSrc" height="180px" />
+
+    <div class="pages-user-header-content">
+      <v-avatar size="130" class="pages-user-header-avatar">
+        <span
+          v-if="iconSrc === true || iconSrc === false"
+          v-text="getAvaterName"
+        />
+        <img v-else :src="iconSrc" :alt="getAvaterAlt" />
+      </v-avatar>
+
+      <div class="text-center">
+        <p v-text="name" />
+        <p v-text="kana" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgSrc: {
+      type: String,
+      required: true
+    },
+
+    iconSrc: {
+      /* Booleanはfalseのみ */
+      type: [String, Boolean],
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    kana: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    getAvaterName() {
+      return this.name.substr(0, 2)
+    },
+
+    getAvaterAlt() {
+      return `${this.name} - Archi Walk アバターアイコン`
+    }
+  }
+}
+</script>
--- pages/pages/_user.vue
+++ pages/pages/_user.vue
@@ -1,21 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <user-header
+      :bgSrc="BgExampleImg"
+      :iconSrc="myIcon"
+      :name="data.name"
+      kana="FURIGANA"
+    />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import UserHeader from '@/components/pages/UserHeader'
+import BgExampleImg from '@/assets/img/page_background_example.png'
+import myIcon from '~/assets/img/usericon-ex.jpg'
 
 export default {
+  components: {
+    UserHeader
+  },
+
   data() {
     return {
-      //
+      BgExampleImg,
+      myIcon
     }
   },
 
   async asyncData({ params, error }) {
     try {
       const { data } = await axios.get(`pages/${params.user}`)
-      return { success: true, data }
+      return { success: true, data: data.data }
     } catch (e) {
       // return error({
       //   statusCode: e.response.data.status,
@@ -23,15 +38,6 @@ export default {
       // })
       return { success: false, statusCode: e.response.data.status }
     }
-  },
-
-  created() {
-    // Fill the form with data.
-    this.form.keys().forEach((key) => {
-      if (this.data[key] !== null) {
-        this.form[key] = this.data[key]
-      }
-    })
   }
 }
 </script>
