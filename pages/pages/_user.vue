a8105d9d7c55bfae1ad933a9abb6efce2d0bb0dc
H37kouya <h37kouya@gmail.com>
2020-03-17 16:49:00 +0900

    いらない記述の削除+コメントアウトの開放

--- pages/pages/_user.vue
+++ pages/pages/_user.vue
@@ -1,89 +1,14 @@
 <template>
-  <div>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      dark
-    >
-      <v-list
-        dense
-        nav
-        class="py-0"
-      >
-        <v-list-item
-          v-for="item in sidebarItems"
-          :key="item.title"
-          link
-        >
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ item.title }}
-            </v-list-item-title>
-            <v-list-item
-              v-for="subtitle in item.subtitles"
-              :key="subtitle.title"
-              link
-            >
-              <v-list-item-title>
-                {{ subtitle.title }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <h1>作品を記録する</h1>
-  </div>
+  <div></div>
 </template>
 
 <script>
 import axios from 'axios'
-// import Form from 'vform'
 
 export default {
   data() {
     return {
-      drawer: true
-      // form: new Form({
-      //   data: ''
-      // })
-    }
-  },
-
-  /*,
-  created () {
-      // Fill the form with data.
-      this.form.keys().forEach((key) => {
-        if (this.data[key] !== null) {
-          this.form[key] = this.data[key]
-        }
-      })
-  }
-  */
-
-  computed: {
-    sidebarItems: () => {
-      return [
-        { title: 'ダッシュボード' },
-        {
-          title: '作品を記録する',
-          subtitles: [
-            { title: 'プロフィールの変更' },
-            { title: '作品の投稿' },
-            { title: '作品一覧' },
-            { title: 'アカウント設定' }
-          ]
-        },
-        {
-          title: '作品を眺める',
-          subtitles: [
-            { title: 'ギャラリー' },
-            { title: 'イイね履歴' },
-            { title: 'お気に入り一覧' }
-          ]
-        }
-      ]
+      //
     }
   },
 
@@ -98,6 +23,15 @@ export default {
       // })
       return { success: false, statusCode: e.response.data.status }
     }
+  },
+
+  created() {
+    // Fill the form with data.
+    this.form.keys().forEach((key) => {
+      if (this.data[key] !== null) {
+        this.form[key] = this.data[key]
+      }
+    })
   }
 }
 </script>
