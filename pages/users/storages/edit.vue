048938ea9c80d92b1eabe5c78dd01fb537fd7de5
toson <t182324@cc.utsunomiya-u.ac.jp>
2020-03-18 12:27:50 +0900

    作品編集概形

--- pages/users/storages/edit.vue
+++ pages/users/storages/edit.vue
@@ -1,87 +1,212 @@
 <template>
   <div>
+    <user-title
+      class="text-center"
+      title="-my work-"
+    />
+
     <!-- TODO 作品情報を修正 -->
-    <v-form @submit.prevent="update" @keydown="form.onKeydown($event)">
-      <!-- title -->
-      <v-text-field
-        v-model="form.title"
-        :counter="255"
-        :label="$t('title')"
-        required
-      />
+    <v-form
+      @submit.prevent="create"
+      @keydown="form.onKeydown($event)"
+    >
+      <v-container class="top">
+        <v-card>
+          <v-card
+            color="#26A69A"
+            dark
+          >
+            <div class="px-4">
+              <div class="d-flex justify-space-between">
+                <div>
+                  <v-card-title>
+                    <v-icon
+                      dark
+                      x-large
+                    >mdi-plus</v-icon>Work
+                  </v-card-title>
+                </div>
 
-      <!-- description -->
-      <v-text-field
-        v-model="form.description"
-        :counter="255"
-        :label="$t('description')"
-        required
-      />
+                <div class="d-flex align-center">
+                  <v-btn
+                    class="teal--text text--lighten-1 mr-4"
+                    color="#CFD8DC"
+                  >
+                    Preview
+                  </v-btn>
 
-      <!-- long_comment -->
-      <v-text-field
-        v-model="form.long_comment"
-        :counter="255"
-        :label="$t('long_comment')"
-        required
-      />
+                  <v-btn
+                    :disabled="form.busy"
+                    class="teal--text text--lighten-1 mr-4"
+                    color="#CFD8DC"
+                    type="submit"
+                  >
+                    {{ $t('store') }}
+                  </v-btn>
+                </div>
+              </div>
+            </div>
+          </v-card>
 
-      <!-- eyecatch_image -->
-      <v-file-input
-        v-model="form.eyecatch_image"
-        :label="$t('eyecatch_image')"
-        @change="eyecatchImageFileChange"
-        accept="image/*"
-        show-size
-        filled
-        prepend-icon="mdi-camera"
-      />
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <!-- title -->
+                <v-text-field
+                  v-model="form.title"
+                  :counter="255"
+                  :label="$t('title')"
+                  required
+                  outlined
+                  clearable
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <!-- web_address -->
+                <v-text-field
+                  v-model="form.web_address"
+                  :counter="255"
+                  :label="$t('web_address')"
+                  required
+                  outlined
+                  clearable
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <!-- 画像のForm -->
+              <v-col cols="7">
+                <h3>{{ $t('eyecatch_image') }}</h3>
 
-      <v-card class="d-inline-block mx-auto">
-        <input
-          @change="onFileChange"
-          type="file"
-          class="file_input"
-          name="photo"
-          accept="image/*"
-        />
-        <div v-if="preview.eyecatch_image">
-          <v-img :src="preview.eyecatch_image" alt="" width="300px" />
-        </div>
+                <div class="pos-relative v-file-input-icon-none">
+                  <!-- eyecatch_image -->
+                  <v-file-input
+                    v-model="form.eyecatch_image"
+                    :label="$t('eyecatch_image')"
+                    @change="eyecatchImageFileChange"
+                    accept="image/*"
+                    show-size
+                    filled
+                    height="200px"
+                  />
 
-        <div v-else-if="form.eyecatch_image_url">
-          <v-img :src="form.eyecatch_image_url" alt="" width="300px" />
-        </div>
+                  <template v-if="preview.eyecatch_image">
+                    <div class="pos-topLeftAlign user_storage_eyecatch_image_preview">
+                      <v-img
+                        :src="preview.eyecatch_image"
+                        alt=""
+                        height="200px"
+                      />
+                    </div>
+                  </template>
 
-        <div v-else>
-          <v-icon x-large>mdi-plus</v-icon>
-        </div>
-      </v-card>
+                  <template v-else-if="form.eyecatch_image_url">
+                    <div class="pos-topLeftAlign user_storage_eyecatch_image_preview">
+                      <v-img
+                        :src="form.eyecatch_image_url"
+                        alt=""
+                        height="200px"
+                      />
+                    </div>
+                  </template>
 
-      <!-- storage -->
-      <v-file-input
-        v-model="form.storage"
-        :label="$t('storage')"
-        show-size
-        filled
-        prepend-icon="mdi-camera"
-      />
+                  <template v-else>
+                    <v-card class="pos-topLeftAlign user_storage_eyecatch_image_empty">
+                      <div
+                        class="pos-relative"
+                        style="height: 200px"
+                      >
+                        <v-icon
+                          class="pos-topAndBottomCenter"
+                          light
+                          x-large
+                        >
+                          mdi-plus
+                        </v-icon>
+                      </div>
+                    </v-card>
+                  </template>
+                </div>
+              </v-col>
 
-      <!-- web_address -->
-      <v-text-field
-        v-model="form.web_address"
-        :counter="255"
-        :label="$t('web_address')"
-        required
-      />
+              <v-col cols="5">
+                <h3>Stl obj fbxファイル</h3>
 
-      <!-- Submit Button -->
-      <div class="text-center login-btn-wraaper">
-        <!-- Submit Button -->
-        <v-btn :disabled="form.busy" color="grey lighten-1" large type="submit">
-          {{ $t('update') }}
-        </v-btn>
-      </div>
+                <div class="pos-relative v-file-input-icon-none">
+                  <!-- storage -->
+                  <v-file-input
+                    v-model="form.storage"
+                    :label="$t('storage')"
+                    show-size
+                    filled
+                    prepend-icon="mdi-camera"
+                    height="200px"
+                  />
+
+                  <v-card class="pos-topLeftAlign user_storage_object_empty">
+                    <div
+                      class="pos-relative"
+                      style="height: 200px"
+                    >
+                      <v-icon
+                        class="pos-topAndBottomCenter"
+                        light
+                        x-large
+                      >
+                        mdi-plus
+                      </v-icon>
+                    </div>
+                  </v-card>
+                </div>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="12">
+                <!-- description -->
+                <v-text-field
+                  v-model="form.description"
+                  :counter="255"
+                  :label="$t('description')"
+                  required
+                  outlined
+                  clearable
+                />
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <!-- long_comment -->
+                <!-- TODO: tinyMCEのようなエディタに置き換えたい -->
+                <v-text-field
+                  v-model="form.long_comment"
+                  :counter="255"
+                  :label="$t('long_comment')"
+                  height="300px"
+                  required
+                  outlined
+                  clearable
+                />
+              </v-col>
+            </v-row>
+
+            <!-- Submit Button -->
+            <div class="text-right login-btn-wraaper">
+              <v-btn
+                :disabled="form.busy"
+                color="grey lighten-1"
+                large
+                type="submit"
+              >
+                {{ $t('store') }}
+              </v-btn>
+            </div>
+          </v-container>
+        </v-card>
+      </v-container>
     </v-form>
   </div>
 </template>
