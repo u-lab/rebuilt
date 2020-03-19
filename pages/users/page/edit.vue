3829e124f93736c213e7097eca25b5e82ac91f20
H37kouya <h37kouya@gmail.com>
2020-03-19 11:28:05 +0900

    formatが聞かないのが気になるので、settings.jsonを修正

--- pages/users/page/edit.vue
+++ pages/users/page/edit.vue
@@ -4,7 +4,7 @@
     <user-title title="ポートフォリオ編集" />
 
     <v-container>
-      <v-card  color="grey lighten-1">
+      <v-card color="grey lighten-1">
         <!-- 画像の挿入 -->
         <div class="pb20">
           <div class="d-flex justify-end pr-3 pt-4">
@@ -22,85 +22,74 @@
           </v-avatar>
         </div>
 
-    <v-row justify="center">
-      <v-col class="pb-0" cols="12">
-      <v-card  color="grey lighten-4">
         <v-row justify="center">
-          <v-col class="pt20" cols="4">
-        <v-text-field
-            label="name"
-            single-line
-          ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row justify="center">
-          <v-col cols="10">
-            <v-textarea outlined>
-               <template v-slot:label>
-                <div>
-                  Bio
-                </div>
-              </template>
-            </v-textarea>
-          </v-col>
-        </v-row>
-        <v-row justify="center">
-          <v-col cols="5">
-            <v-card-title>
-              Career
-            </v-card-title>
-            <v-card height="700px">
-            </v-card>
-          </v-col>
-          <v-col cols="5">
-             <v-card-title>
-              Reword
-            </v-card-title>
-            <v-card height="700px">
+          <v-col class="pb-0" cols="12">
+            <v-card color="grey lighten-4">
+              <v-row justify="center">
+                <v-col class="pt20" cols="4">
+                  <v-text-field label="name" single-line></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="10">
+                  <v-textarea outlined>
+                    <template v-slot:label>
+                      <div>
+                        Bio
+                      </div>
+                    </template>
+                  </v-textarea>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="5">
+                  <v-card-title>
+                    Career
+                  </v-card-title>
+                  <v-card height="700px"> </v-card>
+                </v-col>
+                <v-col cols="5">
+                  <v-card-title>
+                    Reword
+                  </v-card-title>
+                  <v-card height="700px"> </v-card>
+                </v-col>
+              </v-row>
             </v-card>
           </v-col>
         </v-row>
       </v-card>
-      </v-col>
-    </v-row>
-  </v-card>
-</v-container>
-
-
+    </v-container>
 
     <!--debug-->
 
-      <v-form
-        @submit.prevent="updatePage"
-        @keydown="formPage.onKeydown($event)"
-      >
-        <v-text-field
-          v-model="formPage.long_comment"
-          :counter="255"
-          :label="$t('long_comment')"
-          required
-        />
+    <v-form @submit.prevent="updatePage" @keydown="formPage.onKeydown($event)">
+      <v-text-field
+        v-model="formPage.long_comment"
+        :counter="255"
+        :label="$t('long_comment')"
+        required
+      />
 
-        <v-text-field
-          v-model="formPage.masterpiece_storage_id"
-          :counter="255"
-          :label="$t('masterpiece_storage')"
-          required
-        />
+      <v-text-field
+        v-model="formPage.masterpiece_storage_id"
+        :counter="255"
+        :label="$t('masterpiece_storage')"
+        required
+      />
 
-        <div class="text-center login-btn-wraaper">
-          <!-- Submit Button -->
-          <v-btn
-            :disabled="formPage.busy"
-            color="grey lighten-1"
-            large
-            type="submit"
-          >
-            {{ $t('update') }}
-          </v-btn>
-        </div>
-      </v-form>
-    </div>
+      <div class="text-center login-btn-wraaper">
+        <!-- Submit Button -->
+        <v-btn
+          :disabled="formPage.busy"
+          color="grey lighten-1"
+          large
+          type="submit"
+        >
+          {{ $t('update') }}
+        </v-btn>
+      </div>
+    </v-form>
   </div>
 </template>
 
@@ -166,14 +155,14 @@ export default {
 }
 </script>
 <style>
-.pt20{
-  padding-top:170px
+.pt20 {
+  padding-top: 170px;
 }
-.pb20{
+.pb20 {
   padding-bottom: 150px;
 }
 
-.user-icon{
+.user-icon {
   position: absolute;
   left: 50%;
   bottom: 50%;
