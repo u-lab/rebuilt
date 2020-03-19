76196306e3c7530c9d9e642ccac0261ddc43276d
toson <t182324@cc.utsunomiya-u.ac.jp>
2020-03-19 16:58:16 +0900

    アカウント編集作成開始

--- pages/users/profile/edit.vue
+++ pages/users/profile/edit.vue
@@ -1,7 +1,127 @@
 <template>
   <div>
     <!-- TODO 全プロフィール修正 -->
+    <!-- TODO 自分の情報を修正 -->
     <user-title title="プロフィール編集" />
+
+    <v-container>
+      <v-card color="grey lighten-1">
+        <!-- 画像の挿入 -->
+        <div class="pb150">
+          <div class="d-flex justify-end pr-3 pt-4">
+            <v-btn
+              class="teal--text text--lighten-1 "
+              color="grey lighten-3"
+            >
+              Store
+            </v-btn>
+          </div>
+        </div>
+
+        <div class="pos-relative">
+          <!-- eyecatch_image -->
+          <v-file-input
+            v-model="formProfile.eyecatch_image"
+            :label="$t('eyecatch_image')"
+            @change="eyecatchImageFileChange"
+            accept="image/*"
+            show-size
+            filled
+            height="200px"
+          />
+
+          <template v-if="preview.eyecatch_image">
+            <div class=" user_storage_eyecatch_image_preview">
+              <v-avatar
+                class="user-icon"
+                size="280"
+              >
+                <v-img
+                  :src="preview.eyecatch_image"
+                  alt=""
+                />
+              </v-avatar>
+            </div>
+          </template>
+
+          <template v-else-if="formProfile.eyecatch_image_url">
+            <div class=" user_storage_eyecatch_image_preview">
+              <v-avatar
+                class="user-icon"
+                size="280"
+              >
+                <v-img
+                  :src="formProfile.eyecatch_image_url"
+                  alt=""
+                />
+              </v-avatar>
+            </div>
+          </template>
+
+          <template v-else>
+            <v-avatar
+              class="user-icon"
+              size="280"
+              color="grey lighten-1"
+            >
+              <v-icon
+                class="pos-topAndBottomCenter"
+                light
+                x-large
+              >
+                mdi-plus
+              </v-icon>
+            </v-avatar>
+          </template>
+        </div>
+
+        <v-row justify="center">
+          <v-col
+            class="pb-0"
+            cols="12"
+          >
+            <v-card color="grey lighten-4">
+              <v-row justify="center">
+                <v-col
+                  class="pt170"
+                  cols="4"
+                >
+                  <v-text-field
+                    label="name"
+                    single-line
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="10">
+                  <v-textarea outlined>
+                    <template v-slot:label>
+                      <div>
+                        Bio
+                      </div>
+                    </template>
+                  </v-textarea>
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="5">
+                  <v-card-title>
+                    Career
+                  </v-card-title>
+                  <v-card height="700px"> </v-card>
+                </v-col>
+                <v-col cols="5">
+                  <v-card-title>
+                    Reword
+                  </v-card-title>
+                  <v-card height="700px"> </v-card>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
 
     <div>
       <h2>{{ $t('user') }}情報を編集</h2>
@@ -134,6 +254,7 @@ export default {
         hobby: '' /* Stirng */,
         icon_image: '' /* FILE */,
         icon_image_url: '' /* URL */,
+        eyecatch_image: '' /* FILE */,
         job_name: '' /* String */,
         nick_name: '' /* String */,
         web_address: '' /* URL */
@@ -143,7 +264,10 @@ export default {
         name: '' /* String */,
         email: '' /* E-Mail */,
         photo_url: '' /* URL */
-      })
+      }),
+      preview: {
+        eyecatch_image: ''
+      }
     }
   },
 
@@ -205,7 +329,33 @@ export default {
             this.$router.push({ name: 'users.dashboard' })
           })
       } catch (e) {}
+    },
+    eyecatchImageFileChange(e) {
+      // e は FILE Objectであることに注意
+      try {
+        this.preview.eyecatch_image = URL.createObjectURL(e)
+      } catch (e) {
+        this.preview.eyecatch_image = null
+      }
     }
   }
 }
 </script>
+<style>
+.pt170 {
+  padding-top: 170px;
+}
+.pb150 {
+  padding-bottom: 150px;
+}
+
+.user-icon {
+  position: absolute;
+  left: 50%;
+  bottom: 50%;
+
+  transform: translateY(50%) translateX(-50%);
+  -webkit-transform: translateY(50%) translateX(-50%);
+  z-index: 1;
+}
+</style>
