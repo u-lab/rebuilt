5cfdf57bb916d6447c272c4464c924675cceef49
shuwa ishiguri <shuwa0216@yahoo.co.jp>
2020-04-05 10:31:00 +0900

    modify init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 sudo chown ec2-user:ec2-user /home/ec2-user/rebuilt
-cp -r /home/ec2-user/rebuilt/components/ /home/ec2-user/
+sudo cp -r /home/ec2-user/rebuilt/components/ /home/ec2-user/
 
 result=`ps aux | grep node | head -n 1`
 list=(${result// / })
 num=${list[1]}
-echo `kill -9 $num`
+kill -9 $num
 
 cd /home/ec2-user/rebuilt
-echo `cp .env.example .env`
-echo `rm -rf package-lock.json`
-echo `rm -rf node_modules`
-echo `npm install`
-echo `npm run dev ` &
+sudo rm -rf node_modules
+sudo rm -f package-lock.json
+npm install
+npm run dev &
