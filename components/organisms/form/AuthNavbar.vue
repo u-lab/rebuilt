2c4659435bc433895b339210bec09e313059ea04
H37kouya <h37kouya@gmail.com>
2020-04-07 22:23:34 +0900

    いくつか修正
    Delete: /pages/user/list/profile
    Fix Name: organism→organisms
    Fix Name: ~/components → @/components

--- components/organisms/form/AuthNavbar.vue
+++ components/organisms/form/AuthNavbar.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import LocaleDropdown from '~/components/organism/LocaleDropdown'
+import LocaleDropdown from '~/components/organisms/LocaleDropdown'
 import OtherPageLink from '~/components/molecues/auth/OtherPageLink'
 
 export default {
