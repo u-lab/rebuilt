f1483e2a90d76d16df4b14d738535597e1ce622e
H37kouya <h37kouya@gmail.com>
2020-04-08 10:50:46 +0900

    convertToArrayの追加

--- utils/array.js
+++ utils/array.js
@@ -1,1 +1,14 @@
-export const isArray = (value) => Array.isArray(value)
+/**
+ * 配列型に変える
+ *
+ * @param {*} v
+ * @returns Array
+ */
+export const convertToArray = (v) => (v != null ? (isArray(v) ? v : [v]) : [])
+
+/**
+ * 配列型かどうか
+ *
+ * @param {*} v
+ */
+export const isArray = (v) => Array.isArray(v)
