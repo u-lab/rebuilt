49e23e8a4ae709af1efa88e8fc409376822e04f4
H37kouya <h37kouya@gmail.com>
2020-04-10 16:21:47 +0900

    collectCoverageFromの範囲を増やした

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*.js',
+    '<rootDir>/utils/**/*.js'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
