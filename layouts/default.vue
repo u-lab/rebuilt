a9cf2d391fa6fb7522f0c6ae7a3af50ecd07a821
H37kouya <h37kouya@gmail.com>
2020-04-11 23:57:24 +0900

    layouts/defaultの変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
-  <v-app style="background: #ddd">
+  <v-app>
     <navbar />
 
     <!-- Sizes your content based upon application components -->
     <v-content>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-content>
   </v-app>
 </template>
