0493004d2585c739bf14980252f94841171a9434
H37kouya <h37kouya@gmail.com>
2020-04-16 16:41:53 +0900

    default画面にFooterを追加

---
+++ components/atoms/buttons/FooterBtn.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-btn
+      v-if="to"
+      v-text="text"
+      :to="to"
+      color="#fff"
+      text
+      rounded
+      class="my-2 opacity-100"
+      dark
+      nuxt
+    />
+
+    <v-btn
+      v-else-if="href"
+      v-text="text"
+      :href="href"
+      color="#fff"
+      text
+      rounded
+      class="my-2"
+      dark
+      target="_blank"
+      rel="noopener"
+    />
+
+    <v-btn v-else v-text="text" color="#fff" text rounded class="my-2" dark />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: undefined
+    },
+
+    to: {
+      type: [String, Object],
+      default: undefined
+    },
+
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.opacity-100 {
+  &::before,
+  &:focus::before {
+    opacity: 0;
+  }
+
+  &:hover::before {
+    opacity: 0.08;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,15 +7,19 @@
       <!-- Provides the application the proper gutter -->
       <nuxt />
     </v-content>
+
+    <the-default-footer />
   </v-app>
 </template>
 
 <script>
 import Navbar from '@/components/organisms/navbar/DefaultNavbar'
+import TheDefaultFooter from '@/components/organisms/footer/TheDefaultFooter'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    TheDefaultFooter
   }
 }
 </script>
