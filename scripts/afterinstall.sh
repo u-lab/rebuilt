7d783b0d735aea546ce74f9c2a23a7ef22dd2a93
shuwa ishiguri <shuwa0216@yahoo.co.jp>
2020-04-24 12:33:29 +0900

    modified afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo cp -r /home/ec2-user/rebuilt/components/ /home/ec2-user/
+sudo chown -R ec2-user:ec2-user /home/ec2-user
+sudo chown -R ec2-user:ec2-user /home/ec2-user/components
 sudo chmod -R 755 /home/ec2-user/components
 
 result=`ps aux | grep node | head -n 1`
