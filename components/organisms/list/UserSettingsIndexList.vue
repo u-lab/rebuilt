78d146ea4d835b127622449ccf51a2526de14ed4
H37kouya <h37kouya@gmail.com>
2020-05-04 11:25:54 +0900

    SettingIndexを分離

---
+++ components/organisms/list/UserSettingsIndexList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list>
+    <!-- ユーザー名変更 -->
+    <v-list-item :to="{ name: 'users.settings.username' }" nuxt>
+      {{ $t('update_your_user_name') }}
+    </v-list-item>
+
+    <v-divider />
+
+    <!-- メールアドレス変更 -->
+    <v-list-item :to="{ name: 'users.settings.email' }" nuxt>
+      {{ $t('update_your_email_address') }}
+    </v-list-item>
+
+    <v-divider />
+
+    <!-- パスワード変更 -->
+    <v-list-item :to="{ name: 'users.settings.password' }" nuxt>
+      {{ $t('update_your_password') }}
+    </v-list-item>
+
+    <v-divider />
+
+    <v-list-item :to="{ name: 'users.settings.locale' }" nuxt>
+      {{ $t('display_language') }}
+    </v-list-item>
+
+    <v-divider />
+
+    <v-list-item :to="{ name: 'users.settings.deactivate' }" nuxt>
+      {{ $t('deactivate_your_account') }}
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
